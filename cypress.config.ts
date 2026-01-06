7fbfc83b4ec002cae3c705f3c6550fc95b3d7921
DinuraSellapperuma <dinuraslk@gmail.com>
2026-01-06 15:45:57 +0530

    default user and pass change

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
   },
   // default username / password on init
   env: {
-    umami_user: 'admin',
-    umami_password: 'umami',
-    umami_user_id: '41e2b680-648e-4b09-bcd7-3e2b10c06264',
+    umami_user: "dixy",
+    umami_password: "dixy",
+    umami_user_id: "41e2b680-648e-4b09-bcd7-3e2b10c06264",
   },
 });
